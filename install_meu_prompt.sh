69dab902c63f2766182ec1e4078528a939da6ed0
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-14 19:21:43 -0300

    Update install_meu_prompt.sh
    Ajuste para instalar o git se instalar o prompt.

--- install_meu_prompt.sh
+++ install_meu_prompt.sh
@@ -1,5 +1,7 @@
 # Chame esse script antes de todos.
 cp meu_prompt.sh $HOME/.
+sudo apt install -y git
 if [ $(cat $HOME/.bashrc | grep -v ^\# | grep meu_prompt | wc -l) -eq 0 ]; then
    echo 'source $HOME/meu_prompt.sh "yes"' >> $HOME/.bashrc
 fi
+source ~/.bashrc
