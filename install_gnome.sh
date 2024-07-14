325db0412c8f2c195ac392ccded678a9871867ed
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-14 12:44:45 -0300

    Create install_gnome.sh
    Como re-definir os aspectos e definições do Gnome.

---
+++ install_gnome.sh
@@ -0,0 +1,10 @@
+# instala o Gnome Tweaks
+sudo apt install gnome-tweaks
+
+# Reinicia a configuração e restaura a última
+dconf reset -f /org/gnome/
+
+dconf load /org/gnome/ < backup-gnome.dump
+
+#Para fazer o backup execute
+#dconf dump / > backup-gnome.dump
