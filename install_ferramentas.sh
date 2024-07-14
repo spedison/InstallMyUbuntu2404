e04adb801ce5b1e4aac7a4a807766e4ea0ea3b79
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-14 16:27:14 -0300

    Create install_ferramentas.sh
    Muitos pacotes em snap e outros que deveriam estar em snap e não estão.

---
+++ install_ferramentas.sh
@@ -0,0 +1,15 @@
+sudo snap install handbrake-jz
+sudo snap install octave
+sudo snap install guieditor
+sudo snap install labplot
+sudo snap install vpv
+sudo snap install cantor
+sudo snap install keepassxc
+sudo snap install inkscape
+sudo snap install graphs
+sudo snap install rclone
+sudo snap install audacity
+sudo snap install dbeaver-ce
+sudo snap install code
+
+sudo apt install solaar logiops
