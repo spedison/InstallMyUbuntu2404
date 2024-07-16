dc0b2eae1b513def19f59558507b1b4ed2431d01
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-16 09:46:49 -0300

    Update install_cuda.sh
    Add JCuda and other Libs

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,3 +1,9 @@
 sudo apt install nvidia-cuda-toolkit
 ## Reference : https://www.liberiangeek.net/2024/04/install-cuda-on-ubuntu-24-04/
 sudo apt install libcutlass-dev libcutlass-dev libhipblas-doc libhipblas0 
+
+#JCuda
+cd $HOME/git_apps
+git clone https://github.com/jcuda/jcuda.git
+cd jcuda
+git checkout version-12.0.0-RC02
