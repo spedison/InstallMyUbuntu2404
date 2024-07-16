4aeff1d0ace8103b4c590d7e4ef76012d5fa7ee2
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-16 09:41:17 -0300

    Update install_cuda.sh
    Add Reference.

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,2 +1,3 @@
 sudo apt install nvidia-cuda-toolkit
+## Reference : https://www.liberiangeek.net/2024/04/install-cuda-on-ubuntu-24-04/
 sudo apt install libcutlass-dev libcutlass-dev libhipblas-doc libhipblas0 
