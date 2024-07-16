866e9cf80e99996610b75e83bd8a5f34a4440e6d
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-16 12:42:24 -0300

    Update install_cuda.sh
    JDriver atualizado.

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,6 +1,10 @@
-sudo apt install nvidia-cuda-toolkit
+sudo apt install -y nvidia-cuda-toolkit
+
 ## Reference : https://www.liberiangeek.net/2024/04/install-cuda-on-ubuntu-24-04/
-sudo apt install libcutlass-dev libcutlass-dev libhipblas-doc libhipblas0 
+sudo apt install -y libcutlass-dev libcutlass-dev libhipblas-doc libhipblas0 
+
+## Está instalado o driver 535 vendo no nvidia-smi
+sudo apt install -y cuda-drivers-fabricmanager-535
 
 #JCuda
 cd $HOME/git_apps
