96f767295b352fd146ddcf738c89447a5a381a19
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-24 09:10:40 -0300

    Update install_opencv.sh
    Bibliotecas de prerequisito ok e opções de compilação funcionaram com a versão 4.8.1.
    Depois testar com a 4.9.

--- install_opencv.sh
+++ install_opencv.sh
@@ -2,11 +2,29 @@
 sudo apt install -y ffmpeg libffmpeg-nvenc-dev
 
 sudo apt install -y libtesseract-dev libtesseract5 tesseract-ocr-por tesseract-ocr-enm 
-libgtk3-nocsd0 libgtk3.0-cil libgtk3.0-cil-dev libgtk-3-dev libgtkglext1  libgtkglext1-dev libgstreamerd-3-0  libgstreamerd-3-dev 
+sudo apt install -y libgtk3-nocsd0 libgtk3.0-cil libgtk3.0-cil-dev libgtk-3-dev libgtkglext1  libgtkglext1-dev 
+sudo apt install -y libv4l-dev libgstreamerd-3-0  libgstreamerd-3-dev 
+sudo apt install -y libhdf5-dev libhdf5-serial-dev libpbihdf-dev
+sudo apt install -y libeigen3-dev libcxsparse4 libelpa-dev libelpa19 libgsl27 libgslcblas0 libgsl27 libelpa-dev
 
-# Não consegui ainda configurar o Blas decentemente.
-sudo apt install -y libblas-dev liblapack-dev gfortran libatlas-base-dev
+# Não consegui ainda configurar o Blass decentemente.
+sudo apt install -y libblas-dev liblapack-dev gfortran libatlas-base-dev liblapacke-dev
 
+#Baixar a versão 4.8.1 do OpenCV.
+mkdir $HOME/git_apps/opencv/
+cd $HOME/git_apps/opencv/
+git clone 
+git clone
+cd opencv
+git checkout
+cd $HOME/git_apps/opencv/opencv-contrib
+git checkout
+
+#Criar o direorio de build
+mkdir $HOME/git_apps/opencv/build
+cd $HOME/git_apps/opencv/build
+
+# Diretório de destino
 mkdir -p /home/spedison/lib/java/opencv
 
 #Mudar o link simbólico do gcc-13 para o gcc-12 (podemos voltar depois da compilação se necessário)
@@ -21,22 +39,24 @@ sudo rm /usr/bin/g++; ln -s /usr/bin/g++-12 /usr/bin/g++
 ## GTX/RTX Turing – GTX 1660 Ti, RTX 2060, RTX 2070, RTX 2080, Titan RTX, Quadro RTX 4000, Quadro RTX 5000, Quadro RTX 6000, Quadro RTX 8000, Quadro T1000/T2000, Tesla T4
 ## Ref: https://arnon.dk/matching-sm-architectures-arch-and-gencode-for-various-nvidia-cards/
 # Preparar a compilação com CMake
+
 cmake        -D SHELL="/bin/bash -x" \
              -D CMAKE_BUILD_TYPE=RELEASE \
              -D ENABLE_GNU_STL_DEBUG=OFF \
-             -D OPENCV_GENERATE_PKGCONFIG=ON \
              -D BUILD_ZLIB=ON \
              -D WITH_TIFF=ON \
              -D BUILD_JPEG=ON \
              -D BUILD_PNG=ON \
              -D BUILD_WEBP=ON \
+	           -D WITH_OPENCL=ON \
              -D BUILD_SHARED_LIBS=OFF \
              -D ENABLE_FAST_MATH=1 \
-             -D INSTALL_PYTHON_EXAMPLES=ON \
-             -D INSTALL_JAVA_EXAMPLES=ON \
-             -D INSTALL_C_EXAMPLES=ON \
-             -D BUILD_EXAMPLES=ON \
-             -D WITH_GTK=true \
+             -D INSTALL_PYTHON_EXAMPLES=OFF \
+             -D INSTALL_JAVA_EXAMPLES=OFF \
+             -D INSTALL_C_EXAMPLES=OFF \
+             -D BUILD_EXAMPLES=OFF \
+             -D WITH_GTK=false \
+	           -D OPENCV_DNN_OPENCL=OFF \
              -D WITH_TBB=ON \
              -D WITH_OPENMP=OFF  \
              -D WITH_EIGEN=ON \
@@ -68,7 +88,8 @@ cmake        -D SHELL="/bin/bash -x" \
              -D WITH_CUDA=ON \
              -D BUILD_opencv_cudacodec=ON \
              -D WITH_CUDNN=ON \
-             -D OPENCV_DNN_CUDA=ON \
+             -D OPENCV_DNN_CUDA=OFF \
+	           -D CUDA_ARCH_PTX=75 \
              -D CUDA_ARCH_BIN=7.5 \
              -D ARCH=sm_75 \
              -D gencode=arch=compute_75,code=sm_75 \
@@ -90,7 +111,8 @@ cmake        -D SHELL="/bin/bash -x" \
              -D Tesseract_LIBRARY="/usr/lib/x86_64-linux-gnu/libtesseract.so.5" \
              -D Tesseract_DIR="/usr/share/tesseract-ocr/5" \
              -D WITH_TESSERACT=ON \
-             -D BUILD_opencv_viz=ON \
              ../opencv
-             
-make -j 10             
+
+make clean 
+
+make -j 50
