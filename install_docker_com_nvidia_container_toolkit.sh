73d3eb64b157c5b7aa4c2ad752e4881f7194e6bb
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-07-27 19:50:27 -0300

    Update install_docker_com_nvidia_container_toolkit.sh
    Ajustes nos procedimentos

--- install_docker_com_nvidia_container_toolkit.sh
+++ install_docker_com_nvidia_container_toolkit.sh
@@ -2,21 +2,21 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/trusted.gpg.d/docker.asc
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 
-sudo apt-get --allow-unauthenticated install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "DOcker instalado!"
+echo "Docker instalado!"
 
 sudo systemctl status docker
 
 sudo systemctl enable docker
 
-usermod -aG docker spedison
+sudo usermod -aG docker spedison
 
 sudo docker run hello-world
 
