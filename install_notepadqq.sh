653642e803d77b9b32e7b105b710139f32d86278
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2025-02-15 19:21:52 -0300

    Create install_notepadqq.sh

---
+++ install_notepadqq.sh
@@ -0,0 +1,36 @@
+# 1) Remover as versões atuais.
+sudo snap remove notepadqq
+sudo apt remove --purge notepadqq -y
+sudo apt autoremove -y
+
+# 2) Instalar as dependências
+sudo apt update && sudo apt upgrade -y
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+sudo apt update
+sudo apt install -y qtbase5-dev qtchooser qttools5-dev qttools5-dev-tools     libqt5webkit5-dev libqt5svg5-dev libqt5websockets5-dev libqt5webchannel5-dev     qtwebengine5-dev libqt5webenginecore5 libqt5webenginewidgets5
+qmake -v
+conda deactivate 
+hash -r
+which qmake
+export PATH=/usr/lib/qt5/bin:$PATH
+export QT_SELECT=qt5
+qmake -v
+
+# Baixar os fontes do NotepadQQ
+git clone https://github.com/notepadqq/notepadqq.git
+cd notepadqq
+./configure --qmake /usr/lib/qt5/bin/qmake
+
+# Tiveram Alguns Ajustes Manuais
+# vim  src/include/nqqrun.h
+# vim  src/ui/include/nqqrun.h
+# vim src/ui/mainwindow.cpp
+# Compilando e conversando com o ChatGPT.
+make clean
+make -j$(nproc)
+make install
+
+# Não precisei ajustar nada de mais grave no ambiente. 
+# Fiquei preocupado com os novos repositórios, mas pelo que vi não precisei fazer nada que quebrasse o sistema.
+# Mas, o que vi que existe outras opções (mas não iguais ao notepadqq) como : Kate, geany ou mais parrudas com vscode.
