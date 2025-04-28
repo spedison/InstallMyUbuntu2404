f9556df595e9e86d7aa9ee2377efe90728dd7310
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2025-04-28 07:53:12 -0300

    Update install_apps_snap.sh
    Adicionado o Vivaldi, removido os debs e colocado o devido comentário para estudar os apps depois.

--- install_apps_snap.sh
+++ install_apps_snap.sh
@@ -1,15 +1,45 @@
+# Conversor de formatos de vídeo
 sudo snap install handbrake-jz
+
+# Ling programação orientada a matrizes
 sudo snap install octave
+
+# Para programar usando o Octave com 
+# interfaces gráficas amigáveis.
 sudo snap install guieditor
+
+# Gerador de gráficos
+# Deve-se ajustar o acesso a rede dele.
 sudo snap install labplot
+
+# Visualizador de imagens muito leve e simples
 sudo snap install vpv
+
+# Frontend para Octave, Python, R, Máxima.
+# Deve-se instalar o backend.
 sudo snap install cantor
+
+# Cofre de senhas.
 sudo snap install keepassxc
+
+# Editor de imagens vetoriais
 sudo snap install inkscape
+
+# Para fazer gráficos rapaidamente de funções
+# ou mesmo de arquivos, muito bom!
 sudo snap install graphs
+
+# Faz pontos de montagens com serviços de nuvem
 sudo snap install rclone
+
+# Editor de arquivos de audio
 sudo snap install audacity
+
+# Interface gráfica para administração de bancos de dados.
 sudo snap install dbeaver-ce
+
+# Visual Code - IDE
 sudo snap install code
 
-sudo apt install solaar logiops
+# Navegador Web muito bom e leve.
+sudo snap install vivaldi
