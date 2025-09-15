ad17dc428765a857b31949b9ced8e2ef10b9ea44
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2025-09-15 11:23:18 -0300

    Create install_rust.sh

---
+++ install_rust.sh
@@ -0,0 +1,24 @@
+# Refs :: https://en.wikipedia.org/wiki/Rust_(programming_language)
+#         https://www.rust-lang.org
+#         https://wiki.debian.org/Rust
+
+sudo apt update
+
+# Instalação do Curl, se necessário
+sudo apt install -y curl  
+
+# Usa o cur para executar um script remoto para instalação do CURL multiplataforma.
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+#Ambiente que será usado
+rustup default stable
+
+#Documentação local
+rustup docs --book
+
+#Mais algumas ferramentas de análise de código
+rustup component add rust-analyzer rust-src
+
+#TODO: Baixar o livro e converter em PDF.
+
+
